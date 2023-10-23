2f63ea1076e420eb6de0789d941488369d10fb4e
Joris Falter <joris@jorisfalter.com>
2023-10-23 08:21:31 +0200

    x

---
+++ detectSmiles.py
@@ -0,0 +1,20 @@
+# import the required modules
+import cv2
+import matplotlib.pyplot as plt
+from deepface import DeepFace
+
+
+# read image
+img = cv2.imread('clooneyAngry.jpeg')
+
+# call imshow() using plt object
+plt.imshow(img[:, :, :: -1])
+
+# display that image
+plt.show()
+
+# storing the result
+result = DeepFace.analyze(img, actions=['emotion'])
+
+# print result
+print(result)
